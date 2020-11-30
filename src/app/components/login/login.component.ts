cb95cc307c22563d51f0f0598040069197b7269b
huyfr <61008601+huyfr@users.noreply.github.com>
2020-11-30 17:41:14 +0700

    add validate to login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  login(): void {
+    this.submitted = true;
+    if (this.loginForm.valid) {
+      const {username, password} = this.loginForm.value;
+      console.trace("Go into login function");
+      console.info("Login with username: " + username + ", password: " + password);
+    } else {
+      console.error("Cannot login!")
+    }
+  }
+
+  get rfc(): any {
+    return this.loginForm.controls;
   }
 
 }
