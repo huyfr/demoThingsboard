3376a6c1487ce239b63ca73a9f4c36a4de26f586
huyfr <61008601+huyfr@users.noreply.github.com>
2020-12-01 10:29:05 +0700

    delete old template and add new template home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SidebarComponent} from "./components/home/sidebar/sidebar.component";
 
 @NgModule({
@@ -18,7 +18,8 @@ import {SidebarComponent} from "./components/home/sidebar/sidebar.component";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
