19a355cb05f8138dd05773654c5f21d37fee89b7
huyfr <61008601+huyfr@users.noreply.github.com>
2020-12-02 11:10:04 +0700

    edit header in interceptor

--- src/app/authGuard/guard/jwt.interceptor.ts
+++ src/app/authGuard/guard/jwt.interceptor.ts
@@ -17,7 +17,7 @@ export class JwtInterceptor implements HttpInterceptor {
     let currentUser = JSON.parse(localStorage.getItem('currentUser'));
     if (currentUser && currentUser.token) {
       request = request.clone({
-        setHeaders: {Authorization: `Bearer ${currentUser.token}`}
+        setHeaders: {'X-Authorization': 'Bearer ${currentUser.token}'}
       });
     }
     return next.handle(request);
